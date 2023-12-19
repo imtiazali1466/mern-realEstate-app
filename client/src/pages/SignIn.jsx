c883d03050bb8b695b71715b11e3285ea56623ff
imtiazali1466 <imtiazali1466@gmail.com>
2023-12-19 12:35:16 +0500

    image upload functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   signInStart,
-  signInSucess,
+  signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
